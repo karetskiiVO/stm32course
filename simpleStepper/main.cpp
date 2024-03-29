c47d1605390a7b1a16421a05fb24715747a798c4
karetskiiVO <kar.vladi@yandex.ru>
2024-03-29 18:05:27 +0300

    reworked structure

--- simpleStepper/main.cpp
+++ simpleStepper/main.cpp
@@ -1,5 +1,14 @@
 #include <stlib.h>
 
+void systic_delay_ms (uint32_t ms) {
+    SysTick->VAL  = 0;
+    SysTick->LOAD = 8 * ms; // us << 3
+    SysTick->CTRL = 0x1ul;
+
+    for (uint32_t i = 0; i < 1000 * ms; i++)
+        while ((SysTick->CTRL >> 16) & 1);
+}
+
 int main (void) {
     using namespace st;
 
@@ -10,8 +19,11 @@ int main (void) {
 
         for (uint16_t i = 0; i < (1 << 12); i++) {
             st.step();
-            timer::delay_ms(1);
+            systic_delay_ms(1);
+            //timer::delay_ms(1);
         }
+
+        continue;
 
         st.setDirection(Stepper::Direction::CCW);
 
