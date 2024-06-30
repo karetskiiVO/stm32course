080f72f5a4efc6d8bdff086c0174acec412f3ad4
karetskiiVO <kar.vladi@yandex.ru>
2024-06-30 15:58:20 +0300

    started with usb

--- keyboardEmulator/main.cpp
+++ keyboardEmulator/main.cpp
@@ -23,13 +23,74 @@ void configurePin (
     GPIO_TypeDef* GPIOx, uint32_t pin, PinMode pinMode, 
     SpeedMode speedMode, PullMode pullMode, uint32_t alternativeFunc);
 
+const uint32_t Led = 6;
+
+void SysTickInit () {
+    const uint16_t chunk = 60000;
+    SysTick->LOAD = chunk - 1;
+    SysTick->VAL  = chunk - 1;
+    SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
+}
+
+void delayms (uint32_t ms) {
+    const uint16_t chunk = 60000;
+
+    ms *= 1000 / 8;
+
+    for (; ms > chunk; ms -= chunk) {
+        SysTick->CTRL &= ~SysTick_CTRL_COUNTFLAG_Msk;
+        SysTick->VAL = chunk;
+        while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)) {}
+    }
+
+    SysTick->CTRL &= ~SysTick_CTRL_COUNTFLAG_Msk;
+    SysTick->VAL = ms;
+    while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)) {}
+}
+
+void USBInit () {
+    RCC->APB1ENR |= RCC_APB1ENR_USBEN;
+
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+    SYSCFG->PMC  &= ~SYSCFG_PMC_USB_PU;
+
+    USB->CNTR = USB_CNTR_FRES; // Force USB Reset
+    
+    delayms(8);
+    USB->CNTR   = 0;
+    USB->BTABLE = 0;
+    USB->DADDR  = 0;
+    USB->ISTR   = 0;
+    USB->CNTR   = USB_CNTR_RESETM | USB_CNTR_WKUPM;
+
+    NVIC_EnableIRQ(USB_LP_IRQn);
+
+    SYSCFG->PMC |= SYSCFG_PMC_USB_PU;
+}
+
+extern "C" void USB_LP_IRQHandler (void) {
+    GPIOB->ODR |= 1 << Led;
+
+    if (USB->ISTR & USB_ISTR_CTR) {}
+    if (USB->ISTR & USB_ISTR_SOF) {}
+    if (USB->ISTR & USB_ISTR_RESET) {}
+    if (USB->ISTR & USB_ISTR_SUSP) {} // suspend -> still no connection, may sleep
+    if (USB->ISTR & USB_ISTR_WKUP) {} // wakeup
+}
 
 int main () {
-    RCC->APB1ENR |= RCC_APB1ENR_USBEN; // включаем USB
-    
-    USB_ADDR0_TX_ADDR0_TX;
-    USB->DADDR;
+    RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+    configurePin(GPIOB, Led, PinMode::OUTPUT, SpeedMode::LOW, PullMode::NOPULL, 0);
+
+    SysTickInit();
+
+    USBInit();
+    __enable_irq();
+
+    // USB_ADDR0_TX_ADDR0_TX;
+    // USB->DADDR;
     // USB_ADDR0
+
     while (1) {}
 
     return 0;
